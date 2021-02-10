28759527c61cd9ce0690b145f48cdc108079fd74
shuntagami <shuntagami23@gmail.com>
2021-02-10 14:48:28 +0900

    userモデルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -22,12 +22,4 @@ ActiveRecord::Schema.define(version: 2021_02_09_123603) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.integer "age"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
