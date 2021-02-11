8dae2a89235433d8e9fc581415847ae1733d9daa
shuntagami <shuntagami23@gmail.com>
2021-02-11 21:50:27 +0900

    run  bundle exec spring binstub rspec

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "タスク管理機能", type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作成しておく
+      # 作成者がユーザーAであるタスクを作成しておく
+    end
+
+    context 'ユーザーAがログインしているとき'
+    before do
+      # ユーザーAでログインする
+    end
+
+    it 'ユーザーAが作成したタスクが実行される' do
+      # 作成済のタスクの名称が画面上に表示されていることを確認
+    end
+  end
+end
