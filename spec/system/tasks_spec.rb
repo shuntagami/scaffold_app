a8e3b72dd3b84fa3959cb3e58b27f31fca98c8f2
shuntagami <shuntagami23@gmail.com>
2021-02-12 15:09:51 +0900

    タスク表示機能のテスト

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,17 +3,35 @@ require 'rails_helper'
 describe "タスク管理機能", type: :system do
   describe '一覧表示機能' do
     before do
-      # ユーザーAを作成しておく
-      # 作成者がユーザーAであるタスクを作成しておく
+      user_a = create(:user, name: 'ユーザーA', email: 'a@example.com')
+      create(:task, name: '最初のタスク', user: user_a)
     end
 
-    context 'ユーザーAがログインしているとき'
-    before do
-      # ユーザーAでログインする
+    context 'ユーザーAがログインしているとき' do
+      before do
+        visit login_path
+        fill_in 'メールアドレス', with: 'a@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
+      end
     end
 
-    it 'ユーザーAが作成したタスクが実行される' do
-      # 作成済のタスクの名称が画面上に表示されていることを確認
+    context 'ユーザーBがログインしているとき' do
+      before do
+        create(:user, name: 'ユーザーB', email: 'b@example.com')
+        visit login_path
+        fill_in 'メールアドレス', with: 'b@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        expect(page).to have_no_content '最初のタスク'
+      end
     end
   end
 end
