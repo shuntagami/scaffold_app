b9499c671bd6cf4d61fce628c27014d71f5cd425
shuntagami <shuntagami23@gmail.com>
2021-02-14 18:50:47 +0900

    終了期限にNOTNULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_084250) do
+ActiveRecord::Schema.define(version: 2021_02_14_094533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_13_084250) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.date "deadline"
+    t.date "deadline", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
