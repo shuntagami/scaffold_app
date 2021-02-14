f6515bcef5928ba721e6e8e5733c668f0537d149
shuntagami <shuntagami23@gmail.com>
2021-02-14 16:36:55 +0900

    jqueryの導入に成功

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-// import "jquery"
+import "jquery"
 
-require('jquery')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
