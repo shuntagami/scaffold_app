e52ce746bb93ff2719b0ceb9675b6d29e6c59317
shuntagami <shuntagami23@gmail.com>
2021-02-14 12:09:21 +0900

    カラムの型をdatetimeからdateに変更

--- db/migrate/20210213084250_add_column_to_task.rb
+++ db/migrate/20210213084250_add_column_to_task.rb
@@ -1,5 +1,5 @@
 class AddColumnToTask < ActiveRecord::Migration[6.1]
   def change
-    add_column :tasks, :deadline, :datetime
+    add_column :tasks, :deadline, :date
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_13_084250) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.datetime "deadline"
+    t.date "deadline"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
