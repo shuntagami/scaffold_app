7812a0667c01d2e0779079a17e1f178b8848cf49
shuntagami <shuntagami23@gmail.com>
2021-02-17 18:16:10 +0900

    task_paramsを更新
    completedカラムを新たに追加したため

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description, :deadline)
+    params.require(:task).permit(:name, :description, :deadline, :completed)
   end
 
   def set_task
