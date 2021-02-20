f6d6339b59a3a1576e857dbbc4f8895b4b570007
shuntagami <shuntagami23@gmail.com>
2021-02-20 18:32:11 +0900

    存在性の検証

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Task, type: :model do
   it '有効なファクトリを持つこと' do
     expect(task).to be_valid
   end
+
+  describe '存在性の検証' do
+    it '名称がないと登録できないこと' do
+      task.name = ''
+      task.valid?
+      expect(task).to be_valid
+    end
+    it '締め切りがないと登録できないこと' do
+      task.deadline = ''
+      task.valid?
+      expect(task).to_not be_valid
+    end
+  end
 end
