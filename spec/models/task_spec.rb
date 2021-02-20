6b3f9bbaedcd7811c81e4848eb88b15742386b78
shuntagami <shuntagami23@gmail.com>
2021-02-20 18:49:27 +0900

    文字数の検証をテスト

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -19,4 +19,17 @@ RSpec.describe Task, type: :model do
       expect(task.errors[:deadline]).to include('を入力してください')
     end
   end
+
+  describe '文字数の検証' do
+    it '名称が30文字より多いと登録できないこと' do
+      task.name = 'a'*31
+      task.valid?
+      expect(task.errors[:name]).to include('は30文字以内で入力してください')
+    end
+    it '名称が30文字であれば登録できること' do
+      task.name = 'a'*30
+      task.valid?
+      expect(task).to be_valid
+    end
+  end
 end
