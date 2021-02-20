cca08c2276802abb323e974c472c6c8d72f5fcca
shuntagami <shuntagami23@gmail.com>
2021-02-20 18:20:27 +0900

    タスクモデルのファクトリーが有効であることをテスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,10 @@
+require 'date'
+
 FactoryBot.define do
   factory :task do
     name { 'テストを書く' }
     description { 'Rspec&Capybara&FactoryBotを準備する' }
+    deadline { Date.today }
     association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:task) { build(:task) }
+
+  it '有効なファクトリを持つこと' do
+    expect(task).to be_valid
+  end
 end
