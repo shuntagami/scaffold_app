d883f16d62c891f14e879b23c412f3ecc9386530
shuntagami <shuntagami23@gmail.com>
2021-02-28 16:35:36 +0900

    ユーザーデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'date'
+
 User.create!(name: '管理者',
              email: 'admin@example.com',
              password: 'password',
              password_confirmation: 'password',
              created_at: Time.zone.now,
              updated_at: Time.zone.now,
-             admin: true)
+             admin: true,
+            )
+
+10.times do |n|
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password',
+    created_at: Time.zone.now,
+    updated_at: Time.zone.now,
+    admin: false,
+  )
+end
+
+admin_user = User.find(1)
+
+admin_user.tasks.create!(
+  name: 'test',
+  deadline: Date.today,
+)
